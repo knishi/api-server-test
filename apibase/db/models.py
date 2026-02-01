b0be15ebfe04a0df1cbfc47e3eb4f5f830c19f4f
kazuaki <kazuaki@example.com>
2026-02-01 15:50:08 +0900

    Gold Master Feedback: Add CORS support, OPTIONS handling, and to_dict method

--- apibase/db/models.py
+++ apibase/db/models.py
@@ -1,9 +1,18 @@
+import datetime
 from oslo_db.sqlalchemy import models
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String
 
 class _Base(models.ModelBase):
-    pass
+    def to_dict(self):
+        """Simple model to dict conversion."""
+        res = {}
+        for column in self.__table__.columns:
+            value = getattr(self, column.name)
+            if isinstance(value, datetime.datetime):
+                value = value.isoformat()
+            res[column.name] = value
+        return res
 
 Base = declarative_base(cls=_Base)
 
